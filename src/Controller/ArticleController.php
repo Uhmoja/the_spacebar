99468e68cdf0008bfbfd6a8c653b8dad611db91d
Uhmoja <paul.angot1@gmail.com>
2019-07-29 16:29:25 +0200

    Fin du projet

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,36 +4,50 @@
 namespace App\Controller;
 
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
     public function homepage(){
-        return new Response('My first page !');
+        return $this->render('article/homepage.html.twig');
     }
 
     /**
-     * @Route("/news/{slug}")
+     * @Route("/news/{slug}", name="article_show")
      */
-    public function show($slug){
+    public function show($slug, Environment $twigEnvironnement){
         $comments = [
             'I ate a normal rock once. It did NOT taste like bacon!',
             'Woohoo! I\'m going on an all-asteroid diet!',
             'I like bacon too! Buy some from my site! bakinsomebacon.com',
         ];
 
-        dump($slug, $this);
+        //dump($slug, $this);
 
-        return $this->render('article/show.html.twig',[
+        $html = $twigEnvironnement->render('article/show.html.twig',[
             'title' => ucwords(str_replace('-', ' ', $slug)),
             'comments' => $comments,
+            'slug' => $slug,
         ]);
 
+        return new Response($html);
+    }
 
+    /**
+     * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
+     */
+    public function toggleArticleHeart($slug, LoggerInterface $logger){
+        // TODO - actually heart/unheart the article!
+
+        $logger->info('Article is being hearted !');
+
+        return $this->json(['hearts' => random_int(5, 100)]);
     }
 }
