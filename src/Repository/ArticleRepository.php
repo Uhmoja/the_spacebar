698c44d1843ea14114676baa6a0722f58f9ba254
Uhmoja <paul.angot1@gmail.com>
2019-08-26 15:23:50 +0200

    Fin tuto 3 chap 9

--- src/Repository/ArticleRepository.php
+++ src/Repository/ArticleRepository.php
@@ -19,11 +19,10 @@ class ArticleRepository extends ServiceEntityRepository
         parent::__construct($registry, Article::class);
     }
 
-    // /**
-    //  * @return Article[] Returns an array of Article objects
-    //  */
-    /*
-    public function findByExampleField($value)
+    /**
+     * @return Article[]
+     */
+    public function findAllPublishedOrderedByNewest()
     {
         return $this->createQueryBuilder('a')
             ->andWhere('a.exampleField = :val')
@@ -32,9 +31,8 @@ class ArticleRepository extends ServiceEntityRepository
             ->setMaxResults(10)
             ->getQuery()
             ->getResult()
-        ;
+            ;
     }
-    */
 
     /*
     public function findOneBySomeField($value): ?Article
