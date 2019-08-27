b673377e7c8780ce1190a855154c801b4eeb47ee
Uhmoja <paul.angot1@gmail.com>
2019-08-27 14:33:04 +0200

    Fin tuto 3 chap 13

--- public/js/article_show.js
+++ public/js/article_show.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
-    $('.js-like-article').on('click', function (e) {
-       e.preventDefault();
+    $('.js-like-article').on('click', function(e) {
+        e.preventDefault();
 
-       var $link = $(e.currentTarget);
-       $link.toggleClass('fa-heart-o').toggleClass('fa-heart');
+        var $link = $(e.currentTarget);
+        $link.toggleClass('fa-heart-o').toggleClass('fa-heart');
 
-       $.ajax({
-           method: 'POST',
-           url: $link.attr('href')
-       }).done(function(data){
-           $('.js-like-article-count').html(data.hearts);
-       })
+        $.ajax({
+            method: 'POST',
+            url: $link.attr('href')
+        }).done(function(data) {
+            $('.js-like-article-count').html(data.hearts);
+        })
     });
 });
